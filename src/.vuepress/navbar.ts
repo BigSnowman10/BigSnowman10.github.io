81d4989f5c83368c303f29f2739becfd6502db92
BigSnowman10 <felixbingwensun@gmail.com>
2025-04-06 12:12:14 +0200

    Update navbar configuration to add new items and restructure children

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -8,4 +8,34 @@ export default navbar([
     link:  "/posts/cherry.md", 
   },
 
+  {
+    text: "Thing2",
+    icon: "truck",
+    prefix: "/posts/",
+    children: [
+      "banana.md",
+      "kiwi.md",
+      "peach.md",
+      "pear.md",
+      {text: "Thing3", icon: "bell", link: "strawberry.md"},
+    ]
+ 
+    
+  }
+  {
+    text: "Thing4",
+    icon: "face-smile",
+    prefix: "/posts/",
+    children: [
+      {
+        text: "Devider1",
+        icon: "bell",
+        children: [
+          "dragonfruit.md",
+          "tomato.md",
+        ],
+           
+      
+
+
 ]);
