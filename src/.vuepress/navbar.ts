4a9aace4a3a0fd80d9f03aa61b67b37b3b6fbe16
BigSnowman10 <felixbingwensun@gmail.com>
2025-04-06 11:08:10 +0200

    Remove unused navbar items and update link to personal GitHub page

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,48 +1,6 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  "/demo/",
-  {
-    text: "Coding",
-    icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "Ones",
-        icon: "pen-to-square",
-        prefix: "TheOnes/",
-        children: [
-          { text: "TheFirst", icon: "pen-to-square", link: "1" },
-          { text: "TheSecond", icon: "pen-to-square", link: "2" },
-        ],
-      },
-      {
-        text: "More",
-        icon: "pen-to-square",
-        prefix: "more-ones/",
-        children: [
-          {
-            text: "TheMore",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "TheEcenMore",
-            icon: "pen-to-square",
-            link: "2",
-          },  
-        ],
-      },
-      { text: "Cherry", icon: "pen-to-square", link: "cherry" },
-      { text: "Dragon Fruit", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
-  },
-  {
-    text: "Me",
-    icon: "book",
-    link: "https://github.com/BigSnowman10/BigSnowman10.github.io",
-  },
+  "/", 
+
 ]);
