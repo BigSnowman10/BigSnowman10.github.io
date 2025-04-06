434bcd58a9bb1198bdd9e26731a42b78ec11bbcc
BigSnowman10 <felixbingwensun@gmail.com>
2025-04-06 11:26:12 +0200

    Refactor code structure for improved readability and maintainability

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,5 +2,10 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/", 
+  {
+    text: "Thing1",
+    icon: "fa-solid fa-face-smile",
+    link:  "/posts/cherry.md", 
+  },
 
 ]);
